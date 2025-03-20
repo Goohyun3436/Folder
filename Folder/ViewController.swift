809a6c9a2f3e5cec191946d2cb7a29b4edb39a10
Goohyun3436 <3436hyun@gmail.com>
2025-03-20 15:05:35 +0900

    goo3, goo4

--- Folder/ViewController.swift
+++ Folder/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("Hello, goo111")
+        print("Hello, goo121212")
         print("Hello, goo2")
     }
     
